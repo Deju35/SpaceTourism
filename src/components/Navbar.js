ab133f2dd5703f7f3df513aa46275605e386b249
Deju35 <20183556@std.neu.edu.tr>
2022-11-11 19:32:27 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import'./NavbarStyle.css'
+import React, {useState} from 'react'
+
+import { Link } from 'react-router-dom'
+import{FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+    const[click,setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+  return (
+    <div className='header'>
+        <Link to='/'><h1>Space Ody</h1></Link>
+        <ul className={click? 'nav-menu active':'nav-menu'}>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/pricing'>Pricing</Link>
+            </li>
+            <li>
+                <Link to='/training'>Training</Link>
+            </li>
+            <li>
+                <Link to='/contact'>Contact</Link>
+            </li>
+        </ul>
+        <div className='aggMenu' onClick={handleClick}>
+          {click ? (<FaTimes size={20} style={{color:'#fff'}} />):(<FaBars size={20} style={{color:'#fff'}} />)}
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Video.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './VideoStyle.css'
+import {Link} from 'react-router-dom'
+import spaceVideo from '../assets/space.mp4'
+const Video = () => {
+  return (
+    <div className='hero'>
+        <video autoPlay loop mutedd id='video'>
+            <source src={spaceVideo} type='video/mp4'/>
+        </video>
+        <div className='content'>
+            <h1>Galaxy travel</h1>
+            <p>World's First civilian space travel</p>
+            <div>
+            <Link to='/training' className='btn'>Training</Link>
+            <Link to='/contact' className='btn btn-light'>Launch</Link>
+            </div>
+        /</div>
+    </div>
+  )
+}
+
+export default Video
